c78cc8128d1dc3e1415fdf258038960eb04e0d7e
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-05-09 22:19:36 +0900

    Java - Util 패키지(Masking) - 전화번호 마스킹

--- src/makeUtil/masking/Masking.java
+++ src/makeUtil/masking/Masking.java
@@ -27,24 +27,56 @@ public class Masking {
         한글 이름 마스킹 처리
      */
     public String nameKor(String name) {
-        StringBuilder maskingName = new StringBuilder();
+        StringBuilder maskedName = new StringBuilder();
 
         if(name == null || name.isEmpty()){
             return "Name Value is Empty";
         }
 
         // 이름의 첫글자, 마지막 글자 제외 마스킹처리
-        maskingName.append(name.charAt(0));
+        maskedName.append(name.charAt(0));
 
         for (int i = 1; i < name.length() -1; i++) {
-            maskingName.append("*");
+            maskedName.append("*");
         }
-        maskingName.append(name.substring(name.length() - 1));
+        maskedName.append(name.substring(name.length() - 1));
 
-        return maskingName.toString();
+        return maskedName.toString();
     }//nameKor
 
+    /*
+        전화 번호 마스킹
+        핸드폰 - 11자리(344), 10자리(334, OLD)
+     */
+    public String phoneNumber(String number) {
+        StringBuilder maskedNumber = new StringBuilder();
 
+        if(number == null || number.isEmpty()) {return "Number Value Is Empty";}
+
+        if(number.length() == 10){
+            maskedNumber.append(number.substring(0, 3)).append("***");
+            maskedNumber.append(number.substring(number.length() - 4, number.length()));
+
+        }else if(number.length() == 11){
+            maskedNumber.append(number.substring(0, 3)).append("****");
+            maskedNumber.append(number.substring(number.length() - 4, number.length()));
+        }else {
+            maskedNumber.append("Invalid number of digits"); // 유효하지않은 자릿수.
+        }
+
+        return maskedNumber.toString();
+    }
+
+    /*
+        TODO: 계좌번호의 경우 자릿수가 다를시 어떻게 마스킹 할지 설계 후 작성.
+        계좌번호 마스킹
+            - 은행별 계좌번호 자릿수 및 패턴 상이함.
+            -> 11 ~ 13 자리 사이.
+     */
+    public String bankAccount(String account) {
+        String maskedAccount = "";
+        return maskedAccount;
+    }
 
     // For Test
     public static void main(String[] args) {
@@ -56,5 +88,11 @@ public class Masking {
 
         System.out.println("res = " + res);
         System.out.println("res2 = " + res2);
+
+        String numberRes = masking.phoneNumber("01012345678");
+        String numberRes2 = masking.phoneNumber("0112345678");
+        System.out.println("numberRes = " + numberRes);
+        System.out.println("numberRes2 = " + numberRes2);
+
     }
 }
