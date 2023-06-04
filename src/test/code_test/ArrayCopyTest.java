c6a0e136c9486123026301b9e5f748707c572b66
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-06-04 13:38:14 +0900

    Java - array Shallow copy Test

---
+++ src/test/code_test/ArrayCopyTest.java
@@ -0,0 +1,35 @@
+package test.code_test;
+
+/************
+ * @info : 배열 복사 테스트
+ * @name : ArrayCopyTest
+ * @date : 2023/06/04 1:33 PM
+ * @author : SeokJun Kang(swings134@gmail.com)
+ * @version : 1.0.0
+ * @Description :
+ ************/
+public class ArrayCopyTest {
+    public static void main(String[] args) {
+        System.out.println("------ 배열 복사 start ------");
+
+        String[] arr1 = new String[]{"a","b","c","d","e"};
+        String[] arr2 = arr1; // 얕은 복사
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.println("arr1 = " + arr1[i] + " arr2 = " + arr2[i]);
+        }
+
+        System.out.println("-----배열 복사 후 값 변경-----");
+
+        arr1[0] = "aa";	// 동일한 참조값으로 변경을 하기 때문에 iArr1 이나 iArr2 모두 변경된 것으로 보임
+        arr2[3] = "dd";	// 동일한 참조값으로 변경을 하기 때문에 iArr1 이나 iArr2 모두 변경된 것으로 보임
+        for(int i = 0; i < arr1.length; i++) {
+            System.out.println("iArr1[" + i + "]:" + arr1[i] + "|iArr2[" + i + "]:" + arr2[i]);
+        }
+
+        System.out.println("-----참조값 확인-----");
+        System.out.println("arr1 참조값 : " + arr1);
+        System.out.println("arr2 참조값 : " + arr2);
+
+    }//main
+}
