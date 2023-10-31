8a4548b78e916c491ca713acd16f2053aafe5f74
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-11-01 00:38:50 +0900

    Functional interface - test중.....

---
+++ src/functional/test/Functional_test1.java
@@ -0,0 +1,49 @@
+package functional.test;
+
+/************
+ * @info : Functional interface 사용 테스트1
+ * @name : Functional_test1
+ * @date : 2023/10/31 11:32 PM
+ * @author : SeokJun Kang(swings134@gmail.com)
+ * @version : 1.0.0
+ * @Description :
+ ************/
+public class Functional_test1 {
+
+    public static void main(String[] args) {
+        FunctionRun fr = new FunctionRun();
+        fr.start("method start");
+
+        FunctionRun fr2 = new FunctionRun("init param");
+        fr2.start();
+
+    }
+}
+
+interface Run<T> {
+    void start();
+
+    void start(String str);
+
+}
+
+class FunctionRun implements Run<String> {
+
+    String defaultString;
+
+    public FunctionRun() {}
+
+    public FunctionRun(String init) {
+        this.defaultString = init;
+    }
+
+    @Override
+    public void start() {
+        System.out.println(defaultString);
+    }
+
+    @Override
+    public void start(String str) {
+        System.out.println(str);
+    }
+}
---
+++ src/functional/test/Functional_test2.java
@@ -0,0 +1,25 @@
+package functional.test;
+
+public class Functional_test2 {
+    public static void main(String[] args) {
+        boolean init = false;
+
+        // 동작정의 ?
+        CustomFun<Boolean> cf = new CustomFun<Boolean>() {
+            @Override
+            public Boolean test(boolean b) {
+                return b;
+            }
+        };
+
+        Boolean test = cf.test(init);
+        System.out.println("test = " + test);
+
+    }
+}
+
+
+@FunctionalInterface
+interface CustomFun<T> {
+    T test(boolean b);
+}
