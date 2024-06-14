dbc19708403fdb59aaf9f0ee6730775a7901d818
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-06-14 22:46:12 +0900

    add: Java 11 Lambda 파라미터 지역변수 var

---
+++ src/about_11_version/VarExample2.java
@@ -0,0 +1,25 @@
+package about_11_version;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/************
+ * @info : VarExample2
+ * @name : VarExample2
+ * @date : 2024. 6. 14. 오후 10:42
+ * @author : SeokJun Kang(swings134@gmail.com)
+ * @version : 1.0.0
+ * @Description : Lambda 파라미터 지역변수에 var 사용
+ ************/
+public class VarExample2 {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("a", "b", "c");
+        List<String> listParam = list.stream()
+                .map((var s) -> s.toUpperCase())
+                .collect(Collectors.toList());
+
+        System.out.println("listParam = " + listParam);
+    }
+}
