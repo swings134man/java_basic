1153d6000cfa924dffab2fb8e1d674724102e9a4
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2025-01-08 16:48:11 +0900

    feature: Java(POJO) Cache Util
    - Util Class(Thread Safe)
    - Test Class

---
+++ src/main/java/com/lucas/cache/CacheTestMain.java
@@ -0,0 +1,66 @@
+package com.lucas.cache;
+
+/**
+ * @package : com.lucas.cache
+ * @name : CacheTestcom.lucas.java
+ * @date : 2025. 1. 8. 오후 2:55
+ * @author : lucaskang(swings134man)
+ * @Description: Java In-Memory Cache Sample
+ *
+ * - Spring 또는 Library 의 도움을 받지않고 POJO 로 Cache 를 구현하는 방법.
+ * - 간단한 방법으로는 HashMap 을 사용하는 방법이 존재함.
+ * - Multi Thread 환경에서는 동기화 문제가 발생할 수 있으므로, ConcurrentHashMap 을 사용하는 것이 좋다.
+ * - EXP 를 구현하기 위해서는 만료로직에 대한 부분을 추가해야한다.
+**/
+public class CacheTestMain {
+    public static void main(String[] args) throws InterruptedException{
+        ThreadSafeWithExpCache<String, String> cache = new ThreadSafeWithExpCache<>();
+
+        // 기본 expireTime Cache put
+        cache.put("key1", "value1");
+        System.out.println("default Expire Cache : " + cache.get("key1")); // value1
+
+        // expireTime 1초 Cache put
+        cache.put("key2", "value2", 1000);
+        System.out.println("expire 1s Cache : " + cache.get("key2")); // value2
+
+        // expireTime 이 없는 Cache put
+        cache.putWithoutExpire("key3", "value3");
+
+        // expireTime 5초 Cache put
+        cache.put("key4", "value4", 5 * 1000);
+
+        // 5초 대기 --------------------------
+        Thread.sleep(5000);
+        // ---------------------------------
+        System.out.println("------------------------ Expire ------------------------");
+        System.out.println("expire 1s Cache : " + cache.get("key2")); // null
+        // expireTime 이 없는 Cache get
+        System.out.println("No Expire Cache : " + cache.get("key3")); // value3
+        System.out.println("expire 5s Cache : " + cache.get("key4")); // null
+
+        // size
+        System.out.println("size : " + cache.size()); // 2
+
+        System.out.println("------------------------ Utils ------------------------");
+        // remove
+        cache.remove("key1");
+        System.out.println("default Expire Cache : " + cache.get("key1")); // null
+
+        // contains
+        System.out.println("contains key3 not clear : " + cache.containsKey("key3")); // true
+
+        // clear
+        cache.clear();
+        System.out.println("contains key3 : " + cache.containsKey("key3")); // false
+
+        System.out.println("------------------------ ShutDown ------------------------");
+        // Graceful Shutdown
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            public void run() {
+                System.out.println("Shutdown Hook is calling !");
+                cache.shutdown();
+            }
+        });
+    }
+}
---
+++ src/main/java/com/lucas/cache/ThreadSafeWithExpCache.java
@@ -0,0 +1,195 @@
+package com.lucas.cache;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+/**
+ * @package : com.lucas.cache
+ * @name : ThreadSafeWithExpCache.java
+ * @date : 2025. 1. 8. 오후 3:04
+ * @author : lucaskang(swings134man)
+ * @Description: Multi Thread 환경에서 Safe 하고 Expire 기능을 가진 Cache 구현체
+**/
+public class ThreadSafeWithExpCache<K, V> {
+
+    /* ---------------- Constants -------------- */
+
+    /**
+     * Default Expire Time: 1 hour
+     */
+    private static final long DEFAULT_EXPIRE_TIME = 60 * 60 * 1000;
+
+    /* ---------------- Objects  -------------- */
+
+    private static class CacheItem<V> {
+        final V value;
+        final Long expireTime; // if null then never expire Cache
+
+        /**
+         * All Value Constructor
+         * @param value Cache Value {@code V}
+         * @param expireTime Expire Time in {@code Milliseconds}
+         */
+        CacheItem(V value, Long expireTime) {
+            this.value = value;
+            this.expireTime = expireTime;
+        }
+
+        /**
+         * Constructor without Expire Time
+         * @param value Cache Value {@code V}
+         */
+        CacheItem(V value) {
+            this(value, null);
+        }
+    }
+
+    private final ConcurrentHashMap<K, CacheItem<V>> cache = new ConcurrentHashMap<>();
+    private final ScheduledExecutorService scheduler;
+
+    /* ---------------- Constructors  -------------- */
+
+    /**
+     * Default Class Constructor - Create Scheduler with 1 Thread
+     */
+    public ThreadSafeWithExpCache() {
+        this.scheduler = Executors.newScheduledThreadPool(1);
+    }
+
+    /* ---------------- Public operations -------------- */
+
+    /**
+     * Put Cache Item with Default Expire Time
+     *
+     * Default Expire Time is 1 hour
+     *
+     * @param key Cache Key {@code K}
+     * @param value Cache Value {@code V}
+     * @throws NullPointerException if {@code key}, {@code value} is null
+     * @author : lucaskang(swings134man)
+     */
+    public void put(K key, V value) {
+        if(key == null || value == null)
+            throw new NullPointerException();
+
+        long expireTime = System.currentTimeMillis() + DEFAULT_EXPIRE_TIME;
+        cache.put(key, new CacheItem<>(value, expireTime));
+    }
+
+    /**
+     * Put Cache Item with Expire
+     * @param key Cache Key {@code K}
+     * @param value Cache Value {@code V}
+     * @param expireTime Expire Time in {@code Milliseconds}
+     * @throws NullPointerException if {@code key}, {@code value}  is null, or @{code expireTime < 0}
+     * @author : lucaskang(swings134man)
+     */
+    public void put(K key, V value, long expireTime) {
+        if(key == null || value == null || expireTime < 0)
+            throw new NullPointerException();
+
+        long expireTimeMillis = System.currentTimeMillis() + expireTime;
+        cache.put(key, new CacheItem<>(value, expireTimeMillis));
+    }
+
+    /**
+     * Put Cache Item without Expire
+     * @param key Cache Key {@code K}
+     * @param value Cache Value {@code V}
+     * @throws NullPointerException if {@code key}, {@code value} is null
+     * @author : lucaskang(swings134man)
+     */
+    public void putWithoutExpire(K key, V value) {
+        if(key == null || value == null)
+            throw new NullPointerException();
+
+        cache.put(key, new CacheItem<>(value));
+    }
+
+    /**
+     * Get Cache Item by Key
+     * @param key Cache Key
+     * @return if Cache Item is Exist and not Expired return {@code value}
+     * @throws NullPointerException if key is null
+     * @author : lucaskang(swings134man)
+     */
+    public V get(K key) {
+        if(key == null)
+            throw new NullPointerException();
+
+        CacheItem<V> cacheItem = cache.get(key);
+        if(cacheItem == null)
+            return null;
+
+        if(cacheItem.expireTime != null && cacheItem.expireTime < System.currentTimeMillis()) {
+            cache.remove(key);
+            return null;
+        }
+        return cacheItem.value;
+    }
+
+    /**
+     * Check if the Cache contains the Key
+     * @param key Cache Key
+     * @return {@code true} : if this com.lucas.cache contains the key And not Expired
+     * @author : lucaskang(swings134man)
+     */
+    public boolean containsKey(K key) {
+        CacheItem<V> cacheItem = cache.get(key);
+
+        if(cacheItem == null)
+            return false;
+
+        return cacheItem.expireTime == null || cacheItem.expireTime >= System.currentTimeMillis();
+    }
+
+    /**
+     * Remove Cache Item by Key
+     * @param key  Cache Key
+     * @author : lucaskang(swings134man)
+     */
+    public void remove(K key) {
+        cache.remove(key);
+    }
+
+    /**
+     * Clear All Cache Items
+     * @author : lucaskang(swings134man)
+     */
+    public void clear() {
+        cache.clear();
+    }
+
+    /**
+     * return Cache Size
+     * @return {@code int} : Cache Size
+     * @author : lucaskang(swings134man)
+     */
+    public int size() {
+        return cache.size();
+    }
+
+    /**
+     * Shutdown the Scheduler
+     * @author : lucaskang(swings134man)
+     */
+    public void shutdown() {
+        scheduler.shutdown();
+    }
+
+    /* ---------------- Private operations -------------- */
+
+    /**
+     * Clean up Expired Cache Items
+     * @author : lucaskang(swings134man)
+     */
+    private void cleanup() {
+        long now = System.currentTimeMillis();
+        cache.forEach((key, cacheItem) -> {
+            if(cacheItem.expireTime != null && cacheItem.expireTime < now) {
+                cache.remove(key);
+            }
+        });
+    }
+}//end of class
